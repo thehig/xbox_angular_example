83a85a52c0133d30aba6ec53da28f185fb19fd57
Scott O'Malley <scottyomalley@gmail.com>
2013-12-30 13:42:06 +0000

    Added a dependancy in app/modules/profile/controllers.js to

--- app/modules/profile/controllers.js
+++ app/modules/profile/controllers.js
@@ -1,12 +1,31 @@
 // app/modules/profile/controllers.js
 angular.module('profileApp', [])
-.controller('profileCtrl', ['$scope', function ($scope) {
+.controller('profileCtrl', ['$scope', '$http', function ($scope, $http) {
 	
 	$scope.tabs=[];
 
 	$scope.addProfile = function(name){
-		$scope.tabs.push({"name": name});
-		$scope.name = "";//$setPristine();
+	
+		$http.get('https://xboxapi.com/profile/'+name).success(function(data) 
+		{
+			console.log(name)
+			$scope.pushProfile({"name": name, "profile":data, "success": true, "message":"The call returned sucessfully"});
+		})
+		.failure(function(data)
+		{
+			$scope.pushProfile({"name": name, "success": false, "message":"The call didn't return sucessfully"});		
+		});
+
+		$scope.name = "";
+	}
+
+	$scope.pushProfile = function(profile)
+	{	
+		$scope.tabs.push(profile);
+	}
+
+	$scope.getTabs = function()
+	{
+		return $scope.tabs;
 	}
 }]);
-	
