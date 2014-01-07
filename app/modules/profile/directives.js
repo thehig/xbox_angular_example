5388feb0b9c1606c3a6afb63bf33240822388e88
David Higgins <david@vonbismark.com>
2014-01-07 17:27:05 +0000

    Set the my-customer directive to load an external html file for neatness Renamed the customer to gamer-profile Fixed indentation

--- app/modules/profile/directives.js
+++ app/modules/profile/directives.js
@@ -1,7 +1,7 @@
 // app/modules/profile/directives.js
 //takes it's scope frop it's nested controller
 angular.module('profileApp')
-	.directive('myCustomer', [function () {
+	.directive('gamerProfile', [function () {
 		return {
 			restrict: 'E',
 			templateUrl: 'app/modules/profile/views/profile.html'
